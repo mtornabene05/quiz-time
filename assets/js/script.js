ab6f00fa00894f14900ea8118cb7818605399d10
Maria <mtornabene05@gmail.com>
2022-03-30 13:02:16 -0400

    questions created

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+//questions array
+var questions = [
+    {
+        question: "What symbols are used to surround an array in javascript?",
+        answer1: "()",
+        answer2: "[]",
+        answer3: "{}",
+        answer4: "<>"
+    },
+    {
+        question: "How do you reference an ID tag in CSS?",
+        answer1: "*",
+        answer2: ".",
+        answer3: "#",
+        answer4: "?"
+    },
+    {
+        question: "In HTML, how do you reference a javascript?",
+        answer1: "<body>",
+        answer2: "<script>",
+        answer3: "<href>",
+        answer4: "<header>"
+    },
+    {
+        question: "How do you use summon an alert box?",
+        answer1: "alert();",
+        answer2: "msg();",
+        answer3: "alertBox();",
+        answer4: "msgBox();"
+    }
+]
